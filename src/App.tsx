bd3cff6d7195e2df8bd43f29411c1822d8b6c5dd
Ha Ngo <nvth110@gmail.com>
2025-03-13 17:10:02 +0700

    fst page with img

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
+import NavBar from './components/organisms/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div style={{position: 'relative'}}>
+          <img style={{height: '100vh', display: 'block', justifySelf: 'center'}} src='images/thumbnail.jpg' alt="thumbnail" />
+          <div style={{position: 'absolute', backgroundColor: '#3a567350', width: '100%', height: '100%', top: 0, alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', gap: '3%'}}> 
+            <span style={{margin: 0, fontSize: '4rem', fontWeight: 'bold', color: 'white'}}>Hi, I am Ha Ngo </span>
+            <span style={{margin: 0, fontSize: '4rem', fontWeight: 'bold', color: 'white'}}>Full stack Software Engineer</span>
+            <div style={{display: 'flex', flexDirection: 'row', width: '100%', height: '8%', gap: '5%', justifyContent: 'center', alignItems: 'center'}}>
+              <Button style={{backgroundColor:'#f8e5e6', width: '15%', height: '100%', borderRadius: '3rem', color:'#3a567a', fontSize: '1rem', fontWeight: 'bold'}} variant='contained'> Download CV </Button>
+              <Button style={{width: '15%', height: '100%', borderRadius: '3rem', border: "5px solid #f8e5e6", color: '#f8e5e6', fontSize: '1rem', fontWeight: 'bold'}} variant='outlined'> Contact </Button>
+            </div>
+          </div> 
+        </div>
     </div>
   );
 }
